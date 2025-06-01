4a4f6d54ead5209e67715a12a23cf4ae2147324f
Dishant Sakhida <dishantsakhida@gmail.com>
2025-06-02 00:28:07 +0530

    Update BottomTabNavigator.js

--- my-app/src/navigation/BottomTabNavigator.js
+++ my-app/src/navigation/BottomTabNavigator.js
@@ -79,7 +79,7 @@ function TabTwoNavigator() {
       <TabTwoStack.Screen
         name="TabTwoScreen"
         component={TabTwoScreen}
-        options={{ headerTitle: "Tab Two Title" }}
+        options={{ headerTitle: "About" }}
       />
     </TabTwoStack.Navigator>
   );
